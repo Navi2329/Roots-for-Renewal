ffd485da14c4babe5f9666dac4f1779b92177e09
Navaneetha Krishnan <knavaneetha365@gmail.com>
2023-05-18 02:00:46 +0530

    Flask Web App SignUp and Login Functionality added.

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, static_folder='static')
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'lotus@123'
+app.config['MYSQL_DB'] = 'authentication'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    return render_template('index.html')
+
+@app.route('/form_signup', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['txt']
+        email = request.form['email']
+        password = request.form['pswd']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect('/form_login')
+    
+    return render_template('index.html')
+
+@app.route('/form_login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pswd']
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+        user = cur.fetchone()
+        cur.close()
+
+        if user and user[2] == password:
+            return render_template('home.html')  # Render home.html after successful login
+        else:
+            return "Invalid email or password"
+
+    return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
